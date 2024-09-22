a087e93a17f94b5258c5197737bf0db98b853653
X1 Nano <X1 Nano@LAPTOP-B0V7KGGN>
2024-09-22 23:34:32 +0700

    Add project files.

---
+++ 23DH110315_Mau2/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace _23DH110315_Mau2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
